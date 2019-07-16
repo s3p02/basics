8f1dda4e707935dab11bd190db10967cb42c78d2
Sree Sai Sharan Pantangi <sharan_sreesai@myhost.local>
2019-07-16 16:53:12 -0600

    changed inputList to iList

--- pythonLists/listClass.py
+++ pythonLists/listClass.py
@@ -1,31 +1,31 @@
 class lb:
-	def __init__(self,inputList):
-		self.inputList = inputList
+	def __init__(self,iList):
+		self.iList = iList
 	
 	def printList(self):
 		print("___________________________")
 		print("index\t:\telement")
 		print("___________________________")
-		for i in range(len(self.inputList)):
-			print(str(i)+"\t:\t"+self.inputList[i])
+		for i in range(len(self.iList)):
+			print(str(i)+"\t:\t"+self.iList[i])
 	def add2list(self,additionalElement):
-		self.inputList.append(additionalElement)
+		self.iList.append(additionalElement)
 
 	def checkForDuplicates(self):
-		listSet = set(self.inputList)
-		if(len(listSet) != len(self.inputList)):
+		listSet = set(self.iList)
+		if(len(listSet) != len(self.iList)):
 			print("Duplicates Exist\n")
 		else:
 			print("No Duplicates\n")
 
 	def removeDuplicates(self):
-		lbSet = set(self.inputList)
-		del self.inputList[:]
-		self.inputList = list(lbSet)
+		lbSet = set(self.iList)
+		del self.iList[:]
+		self.iList = list(lbSet)
 
 	def removeElement(self,element2remove):
 		#Removes first match
-		self.inputList.remove(element2remove)
+		self.iList.remove(element2remove)
 
 	def removeIndex(self,indexNo):
-		del self.inputList[indexNo]
+		del self.iList[indexNo]
