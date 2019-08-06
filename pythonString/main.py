c7c8bd080e33f8c365514467da8747851af76f54
Sree Sai Sharan Pantangi <sharan_sreesai@myhost.local>
2019-08-06 10:27:56 -0600

    added myAnagrams class

--- pythonString/main.py
+++ pythonString/main.py
@@ -5,5 +5,9 @@ from pythonString import *
 
 if __name__ == "__main__":
     logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
-    st1 = myString("Sree")
+    st1 = myString("Customers")
     logging.debug("RESULT:"+st1.getReverse())
+    name1 = "Customers"
+    name2 = "Store SCUM"
+    myAnagrams(name1,name2).check()
+    myAnagrams("funeral","real fun").check()
--- pythonString/pythonString.py
+++ pythonString/pythonString.py
@@ -1,5 +1,39 @@
 import logging
 import sys
+class myAnagrams:
+    def __init__(self,inputString1,inputString2):
+        logging.debug("myAnagrams CLASS")
+        self.inputString1 = inputString1.replace(" ","").lower()
+        logging.debug("myAnagrams CLASS: inputString1 initialized --> "+self.inputString1)
+        self.inputString2 = inputString2.replace(" ","").lower()
+        logging.debug("myAnagrams CLASS: inputString2 initialized --> "+self.inputString2)
+    
+    def check(self):
+        lenInputString1 = len(self.inputString1)
+        logging.debug("myAnagrams CLASS: check METHOD: Length of Input String 1 --> "+str(lenInputString1))
+        lenInputString2 = len(self.inputString2)
+        logging.debug("\tmyAnagrams CLASS: check METHOD: Length of Input String 2 --> "+str(lenInputString2))
+
+        if lenInputString1 != lenInputString2:
+            logging.debug("myAnagrams CLASS: check METHOD: Checking if the provided strings are of same length or not --> False")
+            return False
+
+        alphabetsRef = "abcdefghijklmnopqrstuvwxyz"
+        inputStringDict1 = dict.fromkeys(alphabetsRef,0)
+        logging.debug("myAnagrams CLASS: check METHOD: Initializing inputStringDict1 for frequency count")
+        inputStringDict2 = dict.fromkeys(alphabetsRef,0)
+        logging.debug("myAnagrams CLASS: check METHOD: Initializing inputStringDict2 for frequency count")
+
+        for i in range(lenInputString1):
+            inputStringDict1[self.inputString1[i]] += 1
+            logging.debug("myAnagrams CLASS: check METHOD: Counting Frequency of "+str(self.inputString1[i])+" inputStringDict1 --> "+str(inputStringDict1[self.inputString1[i]]))
+            inputStringDict2[self.inputString2[i]] += 1
+            logging.debug("myAnagrams CLASS: check METHOD: Counting Frequency of "+str(self.inputString2[i])+" inputStringDict2 --> "+str(inputStringDict2[self.inputString2[i]]))
+        
+        finalResult = inputStringDict1 == inputStringDict2
+        logging.debug("myAnagrams CLASS: check METHOD: Final Result --> "+str(finalResult))
+        return finalResult
+
 class myString:
     def __init__(self,inputString):
         self.inputString = inputString
