7b4c7d4029517dcbf883c869ca676d22986d92ae
Sree Sai Sharan Pantangi <sharan_sreesai@myhost.local>
2019-08-06 09:10:33 -0600

    logging added to pythonString.py, removing print statements

--- pythonString/pythonString.py
+++ pythonString/pythonString.py
@@ -1,26 +1,28 @@
+import logging
 class myString:
     def __init__(self,inputString):
         self.inputString = inputString
-        print("reverseString CLASS: inputString initialized")
+        logging.debug("reverseString CLASS: inputString initialized")
     
     def getReverse(self):
-        print("reverseString CLASS: get function call")
+        logging.debug("reverseString CLASS: get function call")
         lenStr = len(self.inputString)
         strCpy = list(self.inputString)
-        print("reverseString CLASS: get function call:"+str(lenStr))
+        logging.debug("reverseString CLASS: get function call:"+str(lenStr))
         j=0
-        print("reverseString CLASS: get function call:initial\tj="+str(j))
+        logging.debug("reverseString CLASS: get function call:initial\tj="+str(j))
         for i in range(lenStr-1,0,-1):
             temp = strCpy[i]
-            print("reverseString CLASS: get function call:\ttemp="+str(temp))
+            logging.debug("reverseString CLASS: get function call:\ttemp="+str(temp))
             strCpy[i] = strCpy[j]
-            print("reverseString CLASS: get function call:\tinputString["+str(i)+"]="+str(strCpy[i]))
+            logging.debug("reverseString CLASS: get function call:\tinputString["+str(i)+"]="+str(strCpy[i]))
             strCpy[j] = temp
-            print("reverseString CLASS: get function call:\tinputString["+str(j)+"]="+str(strCpy[j]))
+            logging.debug("reverseString CLASS: get function call:\tinputString["+str(j)+"]="+str(strCpy[j]))
             j +=1
-            print("reverseString CLASS: get function call:\tj="+str(j))
+            logging.debug("reverseString CLASS: get function call:\tj="+str(j))
         return ''.join(strCpy)
 
 if __name__ == "__main__":
+    logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
     st1 = myString("Sree")
-    print("RESULT:"+st1.getReverse())
+    logging.debug("RESULT:"+st1.getReverse())
