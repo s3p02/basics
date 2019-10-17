6231b84ec69ab910387cbad903b7add868fe41a5
Sree Sai Sharan Pantangi <sharan_sreesai@myhost.local>
2019-10-17 08:36:52 -0600

    class name changed to Node, counts added to traversal method

--- pythonLinkedList/linkedList.py
+++ pythonLinkedList/linkedList.py
@@ -1,9 +1,12 @@
-class linkedListNode:
+class Node:
     def __init__(self, llnValue):
         self.val = llnValue
         self.next = None
     def traverse(self):
         node = self
+        nodeCount = 0
         while node != None:
-            print(node.val)
+            nodeCount +=1
+            print("Node Value : "+str(node.val))
             node = node.next
+            print("Node-Count: "+str(nodeCount))
---
+++ pythonLinkedList/main.py
@@ -0,0 +1,12 @@
+from linkedList import *
+
+if __name__ == "__main__":
+    node1 = Node(2)
+    node2 = Node(4)
+    node3 = Node(3)
+    node1.next = node2
+    node2.next = node3
+    print("\nFirst Traversal\n")
+    Node.traverse(node1)
+    print("\nSecond Traversal\n")
+    Node.traverse(node1)
