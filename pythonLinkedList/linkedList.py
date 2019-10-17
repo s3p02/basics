345519999c64444f296e824a70f5e1a6de3f8c4f
Sree Sai Sharan Pantangi <sharan_sreesai@myhost.local>
2019-10-17 08:23:06 -0600

    traverse method added to linkedListNode class

--- pythonLinkedList/linkedList.py
+++ pythonLinkedList/linkedList.py
@@ -2,3 +2,8 @@ class linkedListNode:
     def __init__(self, llnValue):
         self.val = llnValue
         self.next = None
+    def traverse(self):
+        node = self
+        while node != None:
+            print(node.val)
+            node = node.next
